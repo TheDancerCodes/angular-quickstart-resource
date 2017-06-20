ca3a57b966a51ba1a3398ef37cf4c3a53f5df1d7
Roger Taracha <rojtaracha@gmail.com>
2017-06-20 17:34:13 +0300

    Implemented Property Binding.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,8 +2,16 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'my-app', // to use me, do this in html <my-app>
-  template: `<h1>Hello {{name}}</h1>`,
+  template: `
+    <h1>{{name}}</h1>
+    <fieldset>
+        <img [src]="image"/>
+    </fieldset>
+    <label [style.color]="color">Favorite Color</label>
+    `,
 })
 export class AppComponent  {
-    name = 'Angular';
+    name = 'Bruz Newton';
+    image = 'favicon.ico';
+    color = 'red';
 }
