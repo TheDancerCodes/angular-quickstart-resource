5f8a2c939e3478b48fb7e928b9b828a7e0e63947
Roger Taracha <rojtaracha@gmail.com>
2017-06-22 17:09:39 +0300

    Models:   + Moving from Component to Data Model.   + Created a data model (model.ts)   + Imported new class (Customer)   + Refactored @Component to accomodate the changes in the AppComponent class.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,43 +1,63 @@
 import { Component } from '@angular/core';
 
+import { Customer } from './model';
+
 @Component({
   selector: 'my-app', // to use me, do this in html <my-app>
   template: `
-    <h1>{{name}}</h1>
+    <h1>{{customer.name}}</h1>
 
-    <p><i>{{name}} is at {{street}} in {{city}} in the {{region}} region.</i></p>
+    <p><i>{{customer.name}} is at {{customer.address.street}} in
+      {{customer.address.city}} City in
+      {{customer.address.county}} County
+      in the {{customer.address.region}} region.</i></p>
     <br/>
 
     <fieldset>
-        <label>Name: <input [(ngModel)]="name"></label>
+        <label>Name: <input [(ngModel)]="customer.name"></label>
     </fieldset>
 
     <label><input type="checkbox" [(ngModel)]="hideAddress">Hide Address</label>
 
     <div [hidden]="hideAddress">
       <fieldset>
-        <label>Street: <input [(ngModel)]="street"></label>
+        <label>Street: <input [(ngModel)]="customer.address.street"></label>
       </fieldset>
       <fieldset>
-        <label>City: <input [(ngModel)]="city"></label>
+        <label>City: <input [(ngModel)]="customer.address.city"></label>
+      </fieldset>
+      <fieldset>
+        <label>County: </label>
+        <select [(ngModel)]="customer.address.county">
+          <option>Kakamega</option>
+          <option>Kiambu</option>
+          <option>Kilifi</option>
+          <option>Turkana</option>
+        </select>
       </fieldset>
       <fieldset>
         <label>Region: </label>
-        <select [(ngModel)]="region">
+        <select [(ngModel)]="customer.address.region">
           <option>north</option>
           <option>south</option>
           <option>east</option>
           <option>west</option>
         </select>
-
       </fieldset>
     </div>
     `,
 })
 export class AppComponent  {
-    name = 'Bruz Newton';
-    city = 'Nairobi';
-    street = 'Koinage Street';
+    customer: Customer = {
+        id: 1,
+        name: 'Bruz Newton',
+        address: {
+          city: 'Nairobi',
+          street: 'Koinage Street',
+          county: 'Kakamega',
+          region: 'east',
+        }
+    };
+
     hideAddress = false;
-    region = 'east';
 }
