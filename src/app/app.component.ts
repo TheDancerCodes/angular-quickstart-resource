aee035aea1d9fcba762cfc80ddbfcde0459e78da
Roger Taracha <rojtaracha@gmail.com>
2017-07-03 13:02:02 +0300

    List Binding:   + List Binding with *ngFor and *ngIf.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,16 +9,52 @@ import { Customer } from './model';
   styleUrls: ['app.component.css']
 })
 export class AppComponent  {
-    customer: Customer = {
-        id: 1,
-        name: 'Bruz Newton',
-        address: {
-          city: 'Nairobi',
-          street: 'Koinage Street',
-          county: 'Kakamega',
-          region: 'east',
-        }
-    };
+    regions = ['North', 'South', 'East', 'West', 'NorthEast'];
+
+    customers: Customer[] = [
+    {
+      id: 1,
+      name: 'Alex Smith',
+      address: {
+        street: '123 Main Street',
+        city: 'Anytown',
+        county: 'Kakamega',
+        region: 'West'
+      }
+    },
+    {
+      id: 2,
+      name: 'Pierre Pasmal',
+      address: {
+        street: '456 Rue de Main',
+        city: 'Quebec City',
+        county: 'Kiambu',
+        region: 'East'
+      }
+    },
+    {
+      id: 3,
+      name: 'Margarita Nadie',
+      address: {
+        street: '789 Calle Principal',
+        city: 'Guadalajara',
+        county: 'Kilifi',
+        region: 'South'
+      }
+    },
+    {
+      id: 4,
+      name: 'Katie O\'Leary',
+      address: {
+        street: '137 DeKoven Street',
+        city: 'Chicago',
+        county: 'Turkana',
+        region: 'NorthEast'
+      }
+    },
+  ];
+
+  customer: Customer; // = this.customers[0];
 
     hideAddress = false;
 }
