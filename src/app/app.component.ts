886642e66a2613483ec1344f62e7f0ff78384ebc
Roger Taracha <rojtaracha@gmail.com>
2017-07-03 11:38:25 +0300

    Refactoring to Template Files:   + Moving Inline Code to Template Files.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,49 +3,10 @@ import { Component } from '@angular/core';
 import { Customer } from './model';
 
 @Component({
+  moduleId: module.id,
   selector: 'my-app', // to use me, do this in html <my-app>
-  template: `
-    <h1>{{customer.name}}</h1>
-
-    <p><i>{{customer.name}} is at {{customer.address.street}} in
-      {{customer.address.city}} City in
-      {{customer.address.county}} County
-      in the {{customer.address.region}} region.</i></p>
-    <br/>
-
-    <fieldset>
-        <label>Name: <input [(ngModel)]="customer.name"></label>
-    </fieldset>
-
-    <label><input type="checkbox" [(ngModel)]="hideAddress">Hide Address</label>
-
-    <div [hidden]="hideAddress">
-      <fieldset>
-        <label>Street: <input [(ngModel)]="customer.address.street"></label>
-      </fieldset>
-      <fieldset>
-        <label>City: <input [(ngModel)]="customer.address.city"></label>
-      </fieldset>
-      <fieldset>
-        <label>County: </label>
-        <select [(ngModel)]="customer.address.county">
-          <option>Kakamega</option>
-          <option>Kiambu</option>
-          <option>Kilifi</option>
-          <option>Turkana</option>
-        </select>
-      </fieldset>
-      <fieldset>
-        <label>Region: </label>
-        <select [(ngModel)]="customer.address.region">
-          <option>north</option>
-          <option>south</option>
-          <option>east</option>
-          <option>west</option>
-        </select>
-      </fieldset>
-    </div>
-    `,
+  templateUrl: 'app.component.html',
+  styleUrls: ['app.component.css']
 })
 export class AppComponent  {
     customer: Customer = {
